4ca657758ac7fb165dd93b0027c2e2b724757037
jrhizor <me@jaredrhizor.com>
2024-04-11 20:56:39 -0700

    add restaurant schema

--- src/data/restaurants.ts
+++ src/data/restaurants.ts
@@ -1,3 +1,16 @@
 export interface Restaurant {
-
+    name: string;
+    street_address: string;
+    city: string;
+    state: string;
+    zip: string;
+    menu_url?: string;
+    takeout: boolean;
+    delivery: boolean;
+    doordash_url?: string;
+    postmates_url?: string;
+    ubereats_url?: string;
+    grubhub_url?: string;
+    other_delivery_url?: string;
+    hours: string[];
 }
